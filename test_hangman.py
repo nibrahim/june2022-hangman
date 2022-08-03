6ce9e8951ce4dc154b2163e4750a599709977a0d
Noufal Ibrahim <noufal@nibrahim.net.in>
2022-08-03 13:12:21 +0530

    Green : Adds test for alpha

--- test_hangman.py
+++ test_hangman.py
@@ -3,3 +3,13 @@ import hangman
 def test_get_random_word_min_length_6():
     word = hangman.get_random_word()
     assert len(word) >= 6
+
+def test_get_random_word_no_non_alphanum():
+    word = hangman.get_random_word()
+    assert word.isalpha()
+
+
+
+# RED - Implement a test that will fail
+# GREEN - Make the test pass
+# REFACTOR - Adjust the code so that all tests pass and code is improved
